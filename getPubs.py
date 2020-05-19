ddf755f2751c2ab67a7cc3d85516a9504e8e67d6
Tom Brien <tom@byth.co.uk>
2020-05-19 16:44:41 +0100

    Build paper list
    Includes gotchas for thesis or pre prints

--- getPubs.py
+++ getPubs.py
@@ -1,10 +1,42 @@
 import scholarly
 
-# Get publications
+def buildLine(pub):
+    tex = r'\item '
+    author = pub.bib['author'].split(' and ')[0].split(' ')[-1]
+    if len(pub.bib['author'].split(' and ')) > 0:
+        author = author + ' et al.'
+    tex += author + r' \textit{'
+    tex += pub.bib['title'] + r'}, '
+    if 'journal' not in pub.bib.keys():
+        pub.bib['journal'] = pub.bib['publisher']
+    tex += pub.bib['journal']
+    if 'volume' in pub.bib.keys():
+        tex += r' \textbf{' + pub.bib['volume'] + r'}'
+    tex += '.'
+    if 'eprint' not in pub.bib.keys():
+        if 'url' in pub.bib.keys():
+            pub.bib['eprint'] = pub.bib['url']
+    tex += r' \mbox{\href{Avaliable online}{' + pub.bib['eprint'] + '}}'
+    return tex
 
+
+thesis = 'http://orca.cf.ac.uk/77064/12/2015BrienPhD%20(1).pdf'
+
+# Get publications
 pubs = scholarly.search_author_id('vGJ6UK8AAAAJ')
-pubs = pubs.fill()
+pubs.fill()
 pubs = pubs.publications
 
-for pub in pubs:
+papers = []
+for i, pub in enumerate(pubs):
     pub.fill()
+    # Skip thesis or preprints
+    if 'eprint' in pub.bib.keys():
+        if pub.bib['eprint'] == thesis:
+            continue
+    if 'journal' in pub.bib.keys():
+        if 'preprint' in pub.bib['journal']:
+            continue
+    papers.append(buildLine(pub))
+
+
